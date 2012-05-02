4aca8529184c4004429aa4f99ce1ef82925370c5
Jens Riemschneider <jens@riemschneider.net>
2012-05-02 06:55:19 +0200

    Initial Commit

---
+++ History/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  History
+//
+//  Created by Jens Riemschneider on 5/2/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
